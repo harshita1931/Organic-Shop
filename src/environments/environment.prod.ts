40345cd2923057412848251bcafbefac5c3538df
Harshita Srivastava <srivastavalike@gmail.com>
2017-09-09 15:03:56 +0530

    Create new project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyAeiEAy-AyQBFJKhq6mW5ekWA0-72J0EsA",
+    authDomain: "oshop-618c9.firebaseapp.com",
+    databaseURL: "https://oshop-618c9.firebaseio.com",
+    projectId: "oshop-618c9",
+    storageBucket: "oshop-618c9.appspot.com",
+    messagingSenderId: "948421733617"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyAeiEAy-AyQBFJKhq6mW5ekWA0-72J0EsA",
+    authDomain: "oshop-618c9.firebaseapp.com",
+    databaseURL: "https://oshop-618c9.firebaseio.com",
+    projectId: "oshop-618c9",
+    storageBucket: "oshop-618c9.appspot.com",
+    messagingSenderId: "948421733617"
+  }
 };
